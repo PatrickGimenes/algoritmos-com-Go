09a4fd234ab071057e9746abf720a13cb616eafa
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2024-02-16 20:39:48 -0300

    fix: correction of the linearSearch function to return the index of the found element

--- linear-search/main.go
+++ linear-search/main.go
@@ -6,20 +6,18 @@ func main() {
 
 	lista := []int{45, 84, 78, 78, 75, 15}
 
-	valor := 15
+	valor := 78
 
 	fmt.Println(linearSearch(lista, valor))
 
 }
 
 func linearSearch(lista []int, v int) int {
-	index := 0
-	for i := 0; i < len(lista); i++ {
+	index := -1
+	for i := 0; i < len(lista); i++ { //itera sobre todos os elementos da lista
 
-		if lista[i] == v {
-			index = i
-		} else {
-			index = -1
+		if lista[i] == v { //verifica se v é igual ao elemento da lista[i]
+			index = i //se for, armazena o index
 		}
 	}
 	return index
